2334b37c9e170967032af376086e80f875ebfe23
Hoang Tri <hoangsidrpr@gmail.com>
2022-12-10 19:27:08 +0700

    Update newest

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const subscriber = require('../models/subscriber')
 const router = express.Router()
 const Subscriber=require('../models/subscriber')
 //Getting all
@@ -11,8 +12,8 @@ router.get('/',async (req,res) => {
     }
 })
 //Getting one
-router.get('/:id',(req,res) => {
-    res.send(req.params.id)
+router.get('/:id', getSubscriber ,(req,res) => {
+    res.send(res.subscriber.name)
 })
 //Creating one
 router.post('/',async (req,res) => {
@@ -35,5 +36,18 @@ router.patch('/:id',(req,res) => {
 router.delete('/:id',(req,res) => {
     
 })
+async function getSubscriber(req, res, next){
+    let subscriber
+    try {
+        subscriber= await Subscriber.findById(req.params.id)
+        if(subscriber == null){
+            return res.status(404).json({message: 'Cannot find subscriber'})
+        }
+    } catch(err) {
+        return res.status(500).json({message: err.message})
+    }
+    res.subscriber = subscriber
+    next()
+}
 
 module.exports = router
